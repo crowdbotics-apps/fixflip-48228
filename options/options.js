b92c91491c4e750403fdc06d6baed57e0b0f0d9c
Crowdbotics <team@crowdbotics.com>
2024-06-03 03:25:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fixflip-48228",
+  url: "https://fixflip-48228.botics.co",
+  api: "https://fixflip-48228.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
